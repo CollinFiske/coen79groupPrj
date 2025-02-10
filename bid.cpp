3fca2dc294d00e0392ec5a56a42c029a73a3ad38
RolandBreise <43227608+RolandBreise@users.noreply.github.com>
2025-02-09 22:31:22 -0800

    Update bid.cpp

--- bid.cpp
+++ bid.cpp
@@ -1,1 +1,25 @@
 
+class bid {
+
+  bid (listing obj){
+  }
+    private:
+    listing listing = obj;
+
+    public:
+
+    void display(){
+      
+    }
+
+    void placeBid(){
+    
+    }
+    void cancelBid(){
+
+    }
+    
+
+};
+
+
