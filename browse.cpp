43c41d2537215fe5612a77cfbd8a1ab2fe9edccc
a1kin777 <jjjyang207@gmail.com>
2025-02-10 14:34:04 +0800

    Update browse.cpp

--- browse.cpp
+++ browse.cpp
@@ -1,1 +1,16 @@
+class browse : public cardealership {
+public:
+  void search(){
+       cout << "Search results for " <<  << "\n";
+       bool found = false;
 
+       for () {
+           if (vehicle->getName().find != ) {
+              cout << vehicle->getName() << "$" << vehicle->getPrice() << "\n"
+                }
+       }
+
+       if (!found) {
+         cout << "No vehicle matching the search.\n";
+       }
+  }
