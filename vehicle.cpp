59a96f26a0f74a164ab99cc2cc3a79f8bc1aaa81
Collin Fiske <132860072+CollinFiske@users.noreply.github.com>
2025-02-09 22:24:27 -0800

    Update vehicle.cpp

--- vehicle.cpp
+++ vehicle.cpp
@@ -2,6 +2,53 @@
 class vehicle {
 
 
+    private:
+        int UUid;
+        int quantity;
+        double buyItNow;
+        double bid;
+        string make;
+        string model;
+        int year;
+        int miles;
+        string condition;
+        
+
+    public:
+    //GETTERS
+        int getUUid(){
+
+        }
+        double getName(){
+
+        }
+        int getQuantity(){
+
+        }
+        double getBIN(){
+
+        }
+        double getBid(){
+            
+        }
+
+    //SETTERS
+        void setUUid(){
+
+        }
+        void setName(){
+
+        }
+        void setQuantity(){
+
+        }
+        void setBIN(){
+
+        }
+        void setBid(){
+            
+        }
+
 
 
 
