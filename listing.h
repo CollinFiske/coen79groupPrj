1d21f0bdf21c9b7a01bf6b13a660eb9e65aa1bf1
Collin Fiske <132860072+CollinFiske@users.noreply.github.com>
2025-02-09 22:41:58 -0800

    Update listing.h

--- listing.h
+++ listing.h
@@ -6,3 +6,56 @@ public:
   
 
 };
+
+
+#include "listing.h"
+
+class listing {
+
+
+    private:
+        int UUid;
+        int quantity;
+        double buyItNow;
+        double bid;
+        
+
+    public:
+    //GETTERS
+        listing search();
+        int getUUid();
+
+        
+        double getName();
+
+        
+        int getQuantity();
+
+        
+        double getBIN();
+
+        
+        double getBid();
+            
+        
+    //SETTERS
+        void setUUid();
+
+        
+        void setName();
+
+        
+        void setQuantity();
+
+        
+        void setBIN();
+
+        
+        void setBid();
+          
+        
+
+
+
+
+};
