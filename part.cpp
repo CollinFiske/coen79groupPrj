113758c9557d3f14c955a56b59b5dd751cab852a
Collin Fiske <132860072+CollinFiske@users.noreply.github.com>
2025-02-09 22:07:32 -0800

    Update part.cpp

--- part.cpp
+++ part.cpp
@@ -1,1 +1,27 @@
 
+class part {
+
+
+    private:
+        int partNumber;
+        double cost;
+        int quantity;
+        double totalCost;
+
+    public:
+        int getPartNumber(){
+
+        }
+        double getCost(){
+
+        }
+        int getQuantity(){
+
+
+        }
+        double getTotalCost(){
+
+        }
+
+
+};
