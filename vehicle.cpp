b23b5c5dce8b4ab8df76bd1fc400bcff963988a1
Collin Fiske <132860072+CollinFiske@users.noreply.github.com>
2025-02-12 18:48:11 -0800

    Update vehicle.cpp

--- vehicle.cpp
+++ vehicle.cpp
@@ -2,6 +2,9 @@
 
 class vehicle : listing {
 
+    vehicle(){
+        
+    }
 
     private:
         
@@ -21,11 +24,11 @@ class vehicle : listing {
         string getCondition(){}
 
     //SETTERS
-        void setMake(){ }
-        void setModel(){  }
-        void setYear(){  }
-        void setMiles(){ }
-        void setCondition(){}
+        void setMake( string m ){ make = m; }
+        void setModel(string m ){ model = m; }
+        void setYear( int y ){ year = y; }
+        void setMiles( int y ){ miles = y; }
+        void setCondition( string c ){ condition = c; }
 
 
 
